6e2bb41e91c8917b4f0c5a92265028fd03e333e1
Akash <akash.chaudhary1306@gmail.com>
2023-10-14 18:24:17 +0200

    Completed the vector_normalizer file. Untested

--- utils/vector_normalizer.py
+++ utils/vector_normalizer.py
@@ -20,6 +20,7 @@ class VectorNormalizer(object):
         sliding_window_size=30,
         n_neighbors=10,
     ):
+        self._embedding_dir = embedding_dir
         self.n_embeddings = n_embeddings
         self.n_dimensions = n_dimensions
         self.sliding_window_size = sliding_window_size
@@ -33,10 +34,6 @@ class VectorNormalizer(object):
         """
         Calculates the average, sd, min and max of all the features across the frames for each embedding sample.
         """
-        average_embeddings = []
-        sd_embeddings = []
-        min_embeddings = []
-        max_embeddings = []
         concatenated_embeddings = []
         for sample in embedding_samples:
             for frame_idx in range(sample.embedding.shape[2]):
@@ -44,50 +41,101 @@ class VectorNormalizer(object):
                 concatenated_embeddings.append(features_per_frame)
 
         concatenated_embeddings = np.array(concatenated_embeddings)
-        average_embeddings = np.mean(concatenated_embeddings, axis=0)
-        sd_embeddings = np.std(concatenated_embeddings, axis=0)
-        min_embeddings = np.min(concatenated_embeddings, axis=0)
-        max_embeddings = np.max(concatenated_embeddings, axis=0)
-        print(f"Average embeddings shape: {average_embeddings.shape}")
-        print(f"SD embeddings shape: {sd_embeddings.shape}")
-        print(f"Min embeddings shape: {min_embeddings.shape}")
-        print(f"Max embeddings shape: {max_embeddings.shape}")
+        self.average_embeddings = np.mean(concatenated_embeddings, axis=0)
+        self.sd_embeddings = np.std(concatenated_embeddings, axis=0)
+        self.min_embeddings = np.min(concatenated_embeddings, axis=0)
+        self.max_embeddings = np.max(concatenated_embeddings, axis=0)
+
         # Plot the average, sd, min and max of all the features across the frames for each embedding sample.
-        self._plot_parameters(
-            average_embeddings, sd_embeddings, min_embeddings, max_embeddings
-        )
+        # self._plot_parameters(
+        # average_embeddings, sd_embeddings, min_embeddings, max_embeddings
+        # )
 
-    def _plot_parameters(
-        self, average_embeddings, sd_embeddings, min_embeddings, max_embeddings
-    ):
-        """
-        Plots the average, sd, min and max of all the features across the frames for each embedding sample.
-        """
-        # Plot the average of all the features across the frames for each embedding sample.
-        plt.figure()
-        plt.title(
-            "Average of all the features across the frames for each embedding sample"
-        )
-        plt.plot(average_embeddings)
-        plt.savefig("average_embeddings.png")
+    # def _plot_parameters(
+    #     self, average_embeddings, sd_embeddings, min_embeddings, max_embeddings
+    # ):
+    #     """
+    #     Plots the average, sd, min and max of all the features across the frames for each embedding sample.
+    #     """
+    #     # Plot the average of all the features across the frames for each embedding sample.
+    #     plt.figure()
+    #     plt.title(
+    #         "Average of all the features across the frames for each embedding sample"
+    #     )
+    #     plt.plot(average_embeddings)
+    #     plt.savefig("average_embeddings.png")
 
-        # Plot the sd of all the features across the frames for each embedding sample.
-        plt.figure()
-        plt.title("SD of all the features across the frames for each embedding sample")
-        plt.plot(sd_embeddings)
-        plt.savefig("sd_embeddings.png")
+    #     # Plot the sd of all the features across the frames for each embedding sample.
+    #     plt.figure()
+    #     plt.title("SD of all the features across the frames for each embedding sample")
+    #     plt.plot(sd_embeddings)
+    #     plt.savefig("sd_embeddings.png")
 
-        # Plot the min of all the features across the frames for each embedding sample.
-        plt.figure()
-        plt.title("Min of all the features across the frames for each embedding sample")
-        plt.plot(min_embeddings)
-        plt.savefig("min_embeddings.png")
+    #     # Plot the min of all the features across the frames for each embedding sample.
+    #     plt.figure()
+    #     plt.title("Min of all the features across the frames for each embedding sample")
+    #     plt.plot(min_embeddings)
+    #     plt.savefig("min_embeddings.png")
 
-        # Plot the max of all the features across the frames for each embedding sample.
-        plt.figure()
-        plt.title("Max of all the features across the frames for each embedding sample")
-        plt.plot(max_embeddings)
-        plt.savefig("max_embeddings.png")
+    #     # Plot the max of all the features across the frames for each embedding sample.
+    #     plt.figure()
+    #     plt.title("Max of all the features across the frames for each embedding sample")
+    #     plt.plot(max_embeddings)
+    #     plt.savefig("max_embeddings.png")
+
+    # Takes the embedding and rertuns the normalized embedding
+    def _normalize_embedding(self, sample, normalization_method="minmax"):
+        if normalization_method == "minmax":
+            normalized_embedding = np.zeros(sample.shape)
+            for i in range(sample.shape[2]):
+                normalized_embedding[:, :, i] = (
+                    2
+                    * (
+                        (sample[:, :, i] - self.min_embeddings)
+                        / (self.max_embeddings - self.min_embeddings)
+                    )
+                    - 1
+                )
+        elif normalization_method == "zscore":
+            normalized_embedding = np.zeros(sample.shape)
+            for i in range(sample.shape[2]):
+                normalized_embedding[:, :, i] = (
+                    sample[:, :, i] - self.average_embeddings
+                ) / self.sd_embeddings
+        return normalized_embedding
+
+    def _save_normalized_embeddings(self, normalized_embedding_path):
+        for root, directories, files in os.walk(self._embedding_dir):
+            for file in files:
+                if file.endswith(".mat"):
+                    mat_file_path = os.path.join(root, file)
+                    data = sio.loadmat(mat_file_path)
+                    embedding = data["embedding"]
+                    normalized_embeddings = np.empty(embedding.shape)
+                    for i in range(embedding.shape[2]):
+                        normalized_embeddings[:, :, i] = self._normalize_embedding(
+                            embedding[:, :, i]
+                        )
+
+                    # Get the parent directory of the mat file
+                    parent_dir = os.path.dirname(self._embedding_dir)
+                    file_name = os.path.basename(file).split(".")[0]
+
+                    # Create the output directory if it doesn't exist
+                    if not os.path.exists(normalized_embedding_path):
+                        os.mkdir(normalized_embedding_path)
+
+                    # Create the output parent directory
+                    new_parent_dir = os.path.join(
+                        normalized_embedding_path,
+                        parent_dir.replace("embeddings_utd_mhad/", "", 1),
+                    )
+                    if not os.path.exists(new_parent_dir):
+                        os.makedirs(new_parent_dir)
+
+                    # Save the mat file to the output directory
+                    new_mat_file_path = os.path.join(new_parent_dir, file_name + ".mat")
+                    sio.savemat(new_mat_file_path, {"embedding": normalized_embeddings})
 
 
 def verify_normalization(normalized_embeddings):
@@ -120,6 +168,7 @@ def main():
     vector_normalizer = VectorNormalizer(embedding_path)
     embedding_samples = vector_normalizer._embedding_samples
     vector_normalizer._calculate_parameters(embedding_samples)
+    normalized_embedding_path = os.path.join(currentdir, "../normalized_embeddings")
     # Calculate the average and sd of each feature across the frames. Each embedding sample might contain different number of frames. So we need to calculate the average and sd of each feature across the frames for each embedding sample.
 
     # Calculate the average and sd of each feature across the frames for each embedding sample
