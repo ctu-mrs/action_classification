474c6bdf3d86289a40e129be95f73aba77453661
Akash <akash.chaudhary1306@gmail.com>
2023-10-14 17:42:54 +0200

    Used Utilities to load embeddings

--- scripts/knn_train_test.py
+++ scripts/knn_train_test.py
@@ -11,16 +11,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import time
+import sys
 
-
-class PoseSample(object):
-    def __init__(self, name, class_name, embedding):
-        self.name = name
-        self.class_name = class_name
-        self.embedding = embedding
-
-    def __repr__(self) -> str:
-        return f"PoseSample(name={self.name}, class_name={self.class_name}, embedding={self.embedding})"
+currentdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(currentdir, "../utils/"))
+from custom_classes import PoseSample, Utilities
 
 
 class ActionClassification(object):
@@ -38,14 +33,10 @@ class ActionClassification(object):
         self.n_dimensions = n_dimensions
         self.sliding_window_size = sliding_window_size
         self.n_neighbors = n_neighbors
-
-        self._embedding_samples, self._class_names = self._load_embedding_samples(
-            embedding_dir, file_extension
+        utils = Utilities()
+        self._embedding_samples, self._class_names = utils._load_embedding_samples(
+            embedding_dir=embedding_dir, file_extension=file_extension
         )
-        print("Embedding samples loaded")
-        # self._ball_tree = self._generateBallTree(leaf_size=leaf_size)
-        # print("Ball Tree Generated")
-        # A custom dtw function using the fast dtw implementation and can be used with sklearn's ball tree
 
     def dtw_distances(self, X, Y):
         """
@@ -78,37 +69,6 @@ class ActionClassification(object):
         This methodes takes a sequence of embeddings and classify the sequence into a class as defined by the class names. It classifies the sequence using a ball tree implementation of knn and uses dtw as a distance metric.
         """
 
-    def _load_embedding_samples(self, embedding_dir, file_extension):
-        """
-        Load .mat files by default. Each file encodes a pose sequence. The dimesions of the file are (n_embeddings, n_dimensions, n_frames).
-
-        The folder structure is assumed to be:
-        embedding_dir
-        ├── class1
-        │   ├── sample1.mat
-        │   ├── sample2.mat
-        │   └── ...
-        ├── class2
-        │   ├── sample1.mat
-        │   ├── sample2.mat
-        │   └── ...
-        └── ...
-
-        """
-
-        embedding_samples = []
-        for root, directories, files in os.walk(embedding_dir):
-            for file in files:
-                if file.endswith(file_extension):
-                    mat_file_path = os.path.join(root, file)
-                    data = sio.loadmat(mat_file_path)
-                    embedding = data["embedding"]
-                    # The class is the name of the sub folder that contains the .mat file
-                    class_name = os.path.basename(root)
-                    embedding_samples.append(PoseSample(file, class_name, embedding))
-        class_names = np.unique([sample.class_name for sample in embedding_samples])
-        return embedding_samples, class_names
-
 
 def main():
     # Get the path to the embeddings
@@ -129,7 +89,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         [sample for sample in action_classifier._embedding_samples],
         [sample.class_name for sample in action_classifier._embedding_samples],
-        test_size=0.2,
+        test_size=0.002,
         random_state=42,
     )
     print("Training")
