41dfc23c62dbb71826259372ce15df95fc09dc25
Akash <akash.chaudhary1306@gmail.com>
2024-03-19 15:46:22 +0100

    Testing raw knn

--- scripts/raw_knn.py
+++ scripts/raw_knn.py
@@ -18,7 +18,40 @@ from dtw import *
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(currentdir, "../utils/"))
-from custom_classes import PoseSample, load_embedding_samples
+from custom_classes import PoseSample
+
+
+def load_embedding_samples(embedding_dir, file_extension="mat"):
+    """
+    Load .mat files by default. Each file encodes a pose sequence. The dimesions of the file are (n_embeddings, n_dimensions, n_frames).
+
+    The folder structure is assumed to be:
+    embedding_dir
+    ├── class1
+    │   ├── sample1.mat
+    │   ├── sample2.mat
+    │   └── ...
+    ├── class2
+    │   ├── sample1.mat
+    │   ├── sample2.mat
+    │   └── ...
+    └── ...
+
+    """
+
+    embedding_samples = []
+    for root, directories, files in os.walk(embedding_dir):
+        for file in files:
+            if file.endswith(file_extension):
+                mat_file_path = os.path.join(root, file)
+                data = sio.loadmat(mat_file_path)
+                embedding = data["d_skel"]
+                # The class is the name of the sub folder that contains the .mat file
+                class_name = os.path.basename(root)
+                embedding_samples.append(PoseSample(file, class_name, embedding))
+    class_names = np.unique([sample.class_name for sample in embedding_samples])
+    print("Embedding samples loaded using Utilities")
+    return embedding_samples, class_names
 
 
 class ActionClassification(object):
@@ -27,7 +60,7 @@ class ActionClassification(object):
         embedding_dir,
         leaf_size=40,
         file_extension="mat",
-        n_embeddings=341,
+        n_embeddings=20,
         n_dimensions=3,
         sliding_window_size=30,
         n_neighbors=10,
@@ -40,24 +73,24 @@ class ActionClassification(object):
             embedding_dir=embedding_dir, file_extension=file_extension
         )
 
-    def dtw_distances(self, args):
+    def dtw_distances(self, X, Y):
         """
         Calculates DTW distances between two samples. The samples are of (t,1,128) shape. The samples are reshaped to (t,128) and then the fastdtw function is used to calculate the distance.
         """
-        X, Y, class_name = args
-        X = np.squeeze(X, axis=1)
-        Y = np.squeeze(Y, axis=1)
-        # X = np.swapaxes(X.reshape(self.n_embeddings * 3, X.shape[2]), 0, 1)
-        # Y = np.swapaxes(Y.reshape(self.n_embeddings * 3, Y.shape[2]), 0, 1)
+        # X = np.squeeze(X, axis=1)
+        # Y = np.squeeze(Y, axis=1)
+        X = np.swapaxes(X.reshape(self.n_embeddings * 3, X.shape[2]), 0, 1)
+        Y = np.swapaxes(Y.reshape(self.n_embeddings * 3, Y.shape[2]), 0, 1)
         # distance, path = fastdtw(X, Y, dist=euclidean, radius=1)
-        distance = dtw(X, Y, keep_internals=True).distance
-        return distance, class_name
+        alignment = dtw(X, Y, keep_internals=True)
+        distance = alignment.distance
+        return distance
 
 
 def main():
     # Get the path to the embeddings
     currentdir = os.path.dirname(os.path.realpath(__file__))
-    embedding_path = os.path.join(currentdir, "../encoded_embeddings/")
+    embedding_path = os.path.join(currentdir, "../utd_mhad_dataset/")
     print("Initializing Action Classifier")
     action_classifier = ActionClassification(embedding_path)
     print("Action Classifier Initialized")
@@ -74,21 +107,16 @@ def main():
     start_time = time.process_time()
     count = 0
     for test_sample in X_test:
-        with Pool(processes=4) as pool:  # Use 4 processes
-            results = pool.map(
-                action_classifier.dtw_distances,
-                [
-                    (
-                        test_sample.embedding,
-                        train_sample.embedding,
-                        train_sample.class_name,
-                    )
-                    for train_sample in X_train
-                ],
+        distances = []
+        for train_sample in X_train:
+            distances.append(
+                action_classifier.dtw_distances(
+                    test_sample.embedding, train_sample.embedding
+                )
             )
-        distances, class_names = zip(*results)
-        y_pred.append(class_names[np.argmin(distances)])
-
+        y_pred.append(y_train[np.argmin(distances)])
+        count += 1
+        print(count)
     end_time = time.process_time()
     print(f"Time taken: {end_time - start_time}")
     print(accuracy_score(y_test, y_pred))
