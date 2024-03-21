9e960e078f940f7615a235a13d56dd15f659486a
Akash <akash.chaudhary1306@gmail.com>
2024-03-21 17:48:34 +0100

    Deleted the build folder

--- build/catkin_generated/action_classification-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(action_classification_MESSAGE_FILES "msg/landmark.msg;msg/IntStamped.msg;msg/landmark3D.msg")
-set(action_classification_SERVICE_FILES "")
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/akash/swarm_ws/src/action_classification/build/devel/include;/usr/include/eigen3".split(';') if "/home/akash/swarm_ws/src/action_classification/build/devel/include;/usr/include/eigen3" != "" else []
-PROJECT_CATKIN_DEPENDS = "geometry_msgs;std_srvs;nav_msgs;roscpp;rospy;sensor_msgs;std_msgs;mrs_msgs;mrs_lib;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "action_classification"
-PROJECT_SPACE_DIR = "/home/akash/swarm_ws/src/action_classification/build/devel"
-PROJECT_VERSION = "0.0.0"
